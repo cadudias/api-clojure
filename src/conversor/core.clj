29bbc0bbba9713f927f6ce1ff4a9ce5daabbc4f5
Ricardo Dias <cadu.hd@gmail.com>
2019-10-15 07:07:46 -0300

    initial commit

---
+++ src/conversor/core.clj
@@ -0,0 +1,8 @@
+(ns conversor.core
+  (:gen-class))
+
+;; -main = método estatico
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args] ;; [& args] & = quantidade indefinida de argumentos
+  (println "Hello, World!"))
